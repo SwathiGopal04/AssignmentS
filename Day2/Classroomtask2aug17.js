d048c84f1c2151d863e668f96da2aea7cbbd3cad
Swathi Gopal <swathigopal04@gmail.com>
2025-08-19 10:41:33 +0530

    Add files via upload

---
+++ Day2/Classroomtask2aug17.js
@@ -0,0 +1,22 @@
+//print num from 20 to 30 only even Number
+
+for(i=20;i<=30;i++)
+    {
+    if(i%2===0){
+     console.log("The even number is " +i)
+    }        
+}
+
+
+let browsername= 'chrome'
+let browsern= new String('chrome')
+console.log(browsername===browsern)
+
+console.log(browsername.length)
+console.log(browsername.charAt(4))//ses of posistion of character
+console.log(browsername.indexOf(o)) //fetch index of the character
+console.log("Launching browser" +browsername)
+console.log(`Launching browser ${browsername}`)// used to concat using backtick
+console.log(browsername.split(""))
+console.log(browsername.split(''))
+
